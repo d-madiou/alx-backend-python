faebd9423f550362e062c117a8d7e1863c84fdb6
d-madiou <madioudiallo846@gmail.com>
2025-06-08 11:36:45 +0800

    The last

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,5 +1,7 @@
 # let's do the pagination for the chats
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
 class ChatPagination(PageNumberPagination):
     """
     Custom pagination class for chat messages.
@@ -8,10 +10,14 @@ class ChatPagination(PageNumberPagination):
     page_size_query_param = 'page_size'  # Allow clients to set the page size
     max_page_size = 100  # Maximum allowed page size
     page_query_param = 'page'  # Query parameter for the page number
-    
+
     def get_paginated_response(self, data):
         """
         Override to include additional metadata in the paginated response.
         """
-        return super().get_paginated_response(data)
-    
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data,
+        })
