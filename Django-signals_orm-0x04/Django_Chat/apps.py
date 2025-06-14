e932e60cb3f0cc4ba16a96d74a1c7037a633d84d
d-madiou <madioudiallo846@gmail.com>
2025-06-14 09:50:21 +0800

    The new release

--- Django-signals_orm-0x04/Django_Chat/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoChatConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Django_Chat'
-def ready(self):
-    import Django_Chat.signals
-    import messaging.signals
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Signal to create notifications on new messages."""
-from django.db.models.signals import post_save
+import contextlib
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, MessageHistory, Notification
 
 @receiver(post_save, sender=Message)
 def create_notification_on_message(sender, instance, created, **kwargs):
@@ -11,3 +12,14 @@ def create_notification_on_message(sender, instance, created, **kwargs):
     """
     if created:
         Notification.objects.create(user=instance.receiver, message=instance)
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.id:
+        with contextlib.suppress(Message.DoesNotExist):
+            old_message = Message.objects.get(id=instance.id)
+            if old_message.content != instance.content:
+                MessageHistory.objects.create(
+                    message=instance,
+                    old_content=old_message.content
+                )
+                instance.edited = True
